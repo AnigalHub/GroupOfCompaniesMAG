13de35fe3dc2d2522aa42dc0093c18969767ed8b
AnigalHub <anigal1997@gmail.com>
2022-11-24 21:34:24 +0300

    добавлены новые компоненты и общие стили

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <sidebar-menu/>
+      <Home/>
+      <About/>
+      <Advantages/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import SidebarMenu from "@/components/SidebarMenu.vue"
+    import Home from './components/Home.vue'
+    import About from "@/components/About";
+    import Advantages from "@/components/Advantages";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+      Advantages,
+      About,
+    Home, SidebarMenu,
   }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    height: 100%;
+    margin: 0 !important;
+    padding: 0 !important;
+    background: #edf8fc;
+}
+/*контейнер на блоки посередине экрана*/
+.container{
+    padding-right: 15px !important;
+    padding-left: 15px !important;
+}
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+button{
+    cursor: pointer;
+    appearance: none;
+    border: none;
+    outline: none;
+    background: none;
+}
+h2{
+    text-transform: uppercase;
+    text-shadow: 1px 1px 2px #000;
+    color: #77AEDB;
+    font-size: 2.5rem !important;
 }
 </style>
