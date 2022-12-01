a6b465e16a30a3e0127f1feb0958cd0a8917ec52
AnigalHub <anigal1997@gmail.com>
2022-12-01 22:10:03 +0300

    компонент про сотрудников

---
+++ src/components/Employees.vue
@@ -0,0 +1,71 @@
+<template>
+    <div id="Employees">
+        <b-container>
+            <b-row>
+                <b-col>
+                    <component :is="svg"/>
+                </b-col>
+                <b-col>
+                    <h2>Сотрудники <span>, которым доверяют</span></h2>
+                    <ul v-for="list in List" class="ul">
+                        <li class="text">
+                           <span>{{list.name}}</span>
+                            <br/>
+                            {{list.text}}
+                        </li>
+                    </ul>
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
+</template>
+
+<script>
+    import AboutSvg from "./svg/AboutSvg";
+    export default {
+        name: "Employees",
+        data() {
+            return {
+                svg: AboutSvg,
+                List:[
+                    {name:"Работники с высшим образованием",text:"У нас работают сотрудники только с высшим образованием"},
+                    {name:"Аттестация каждые полгода",text:"Каждые полгода все сотрудники проходят подготовку и аттестацию в учебном центре"},
+                    {name:"Только славяне",text:"В компании работают только славяне. Каждый из них дорожит своей репутацией и имеет профессиональную подготовку"},
+                ],
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    #Employees{
+        padding: 4% 0 4%;
+        .ul {
+            padding:0;
+            list-style: none;
+        }
+        .ul li:before {
+            padding-right:10px;
+            font-size: 2rem;
+            font-weight: bold;
+            color: #77AEDB;
+            content: "\2606";
+            transition-duration: 0.5s;
+        }
+        .ul li:hover:before {
+            color: #337AB7;
+            content: "\2605";
+        }
+        p,li,.text{
+            font-size: 1.4rem;
+            line-height: 2rem;
+        }
+        h2 span{
+            text-shadow: none;
+            text-transform: lowercase;
+        }
+        li span{
+            font-weight: 600;
+        }
+    }
+</style>
