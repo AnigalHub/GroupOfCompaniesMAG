679d44c669d262fa58f49413ad83c9e0572d43cb
AnigalHub <anigal1997@gmail.com>
2022-12-08 20:29:43 +0300

    изменение преимуществ по компании (стили, список)

--- src/components/Advantages.vue
+++ src/components/Advantages.vue
@@ -4,11 +4,11 @@
             <b-row>
                 <b-col>
                     <h2>Почему выбирают нас</h2>
-                    <ul v-for="list in List" class="ul">
-                        <li class="text">{{list.text}}</li>
+                    <ul class="ul">
+                        <li v-for="list in List" class="text">{{list.text}}</li>
                     </ul>
                 </b-col>
-                <b-col>
+                <b-col cols="6">
                     <component :is="svg"/>
                 </b-col>
             </b-row>
@@ -17,16 +17,17 @@
 </template>
 
 <script>
-    import AdvantagesSvg from "./svg/AdvantagesSvg";
+    import HomeSvg from "./svg/HomeSvg";
     export default {
         name: "Advantages",
         data(){
             return{
-                svg:AdvantagesSvg,
+                svg:HomeSvg,
                 List:[
                     {text:"Безупречная репутация",},
-                    {text:"Личная сдача результата работы контролирующим органам",},
                     {text:"Лучший технадзор на рынке",},
+                    {text:"Выполнение всех работ точно в срок",},
+                    {text:"Личная сдача результата работы контролирующим органам",},
                 ],
             }
         }
@@ -35,38 +36,38 @@
 
 <style scoped lang="scss">
     #Advantages{
-        padding: 4% 0 4%;
+        padding: 4% 3% 0;
         .ul {
             padding:0;
             list-style: none;
+        }
+        .ul li{
+           font-weight: 600;
+            padding-top: 2%;
+            padding-bottom: 3%;
+            padding-left: 2%;
         }
         .ul li:before {
             padding-right:10px;
             font-size: 2rem;
             font-weight: bold;
             color: #77AEDB;
-            content: "\2606";
+            content: "\1F5F8";
             transition-duration: 0.5s;
         }
         .ul li:hover:before {
             color: #337AB7;
-            content: "\2605";
-        }
-        .col .flex-container > ul{
-            width: 46%;
-            margin: 1% 1.5% 1%;
-        }
-        .flex-container > ul {
-            width: 22%;
-            margin: 0 1% 6%;
-        }
-        .text{
-            font-size: 1.45rem;
-            line-height: 2rem;
+            content: "\2713";
         }
         svg{
             width: 100%;
-            margin-bottom: 3%;
+            margin-top: -5%;
+            margin-bottom: 10%;
+        }
+        .col-1 svg{
+            width: 2.8rem;
+            margin-top: 45%;
+            margin-right: 15%;
         }
     }
 </style>
