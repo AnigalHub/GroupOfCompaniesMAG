d31350f79c134793616ed407789b042edf85cfe8
AnigalHub <anigal1997@gmail.com>
2022-12-09 15:57:25 +0300

    Добавлен состав компании, изменены стили на направления

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,23 +2,23 @@
     <div id="About">
         <b-container>
             <b-row>
-                <b-col cols="6">
+                <b-col cols="5">
                     <component :is="svg"/>
                 </b-col>
                 <b-col>
                     <h2>О нас</h2>
-                    <p>
-                        <b>ОАО "Группа компаний МАГ"</b>- группа компаний, занимающаяся строительно-монтажными и пуско-наладочными работами систем:
-                    </p>
-                    <div class="flex-container">
-                        <ul v-for="list in FirstList" class="ul">
-                            <li class="text">{{list.text}}</li>
-                        </ul>
-                    </div>
+                    <p><b>ОАО "Группа компаний МАГ"</b> занимается строительно-монтажными и пуско-наладочными работами систем.</p>
+                    <p><u>Наша компания состоит из:</u></p>
+                    <ul>
+                        <li><b>ООО "МРС"</b> - оператор связи и проектирование;</li>
+                        <li><b>ООО "Аваконт"</b> - строительно-монтажные работы;</li>
+                        <li><b>ООО "Гостехносервис"</b> - локальные и международные поставки.</li>
+                    </ul>
                 </b-col>
             </b-row>
+            <p style="text-align: center"><u>Основные направления компании:</u></p>
             <div class="flex-container">
-                <ul v-for="list in SecondList" class="ul">
+                <ul v-for="list in FirstList" class="ul">
                     <li class="text">{{list.text}}</li>
                 </ul>
             </div>
@@ -38,8 +38,6 @@
                     {text:"Дератизационные системы (ОЗДС) ",},
                     {text:"Пожарные сигнализации (СПС, АПС)",},
                     {text:"Контроля и управления доступом (СКУД)",},
-                ],
-                SecondList:[
                     {text:"Контроля учета электроэнергии (АСКУЭ)",},
                     {text:"Охранные комплексы и различный мониторинг",},
                     {text:"Проводного радиовещания (РСО, РАСЦО)",},
@@ -60,43 +58,44 @@
         flex-wrap: wrap;
     }
     #About{
-        h2{
-            margin-top: 5%;
-            margin-left: -8%;
-        }
         padding: 4% 3% 4%;
+        h2{
+            margin-top: 8%;
+        }
         .ul {
             padding:0;
-            list-style: none;
-            margin-bottom: .8rem !important;
+            //list-style: none;
+            margin-bottom: .5rem !important;
             font-size: 1.2rem;
         }
-        .ul li:before {
-            padding-right:10px;
-            font-size: 2rem;
-            font-weight: bold;
-            color: #77AEDB;
-            content: "\2999";
-            transition-duration: 0.5s;
+        .col{
+            ul {
+                list-style: none !important;
+            }
+            li:before {
+                padding-right:10px;
+                margin-left: -3%;
+                font-size: 2rem;
+                color: #77AEDB;
+                content: "\2727";
+            }
+            li:hover:before {
+                color: #337AB7;
+                content: "\2726";
+            }
         }
-        .col .flex-container{
-            margin-left: -6%;
-        }
-        .col .flex-container > ul{
-            width: 100%;
-            margin: 1% 1.5% 1%;
-        }
+
         .flex-container > ul {
-            width: 46%;
-            margin: 0 1% 2%;
-        }
-        p{
-            margin-top: 2%;
-            margin-bottom: 2%;
-            margin-left: -8%;
+            width: 32%;
+            list-style: none;
+            padding: 1%;
+            margin: .5%;
+            box-shadow: 0 3px 6px -2px rgb(0 0 0 / 10%);
+            border: 2px solid #b7dfe9;
+            border-radius: 12px;
         }
         svg{
-            width: 95%;
+            width: 100%;
             margin-bottom: 3%;
         }
     }
