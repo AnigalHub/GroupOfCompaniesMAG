44f136067092955d14a16f42f6e64466291babc8
AnigalHub <anigal1997@gmail.com>
2022-12-10 19:40:38 +0300

    изменена фраза у кнопки

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -7,7 +7,7 @@
                     <ul v-for="list in List" class="ul">
                         <li class="text">{{list.text}}</li>
                     </ul>
-                    <b-button><a>Узнать цены на предоставляемые виды работ</a></b-button>
+                    <b-button><a>Узнать цены на виды работ</a></b-button>
                 </b-col>
                 <b-col>
                     <component :is="svg"/>
@@ -47,13 +47,13 @@
             width: 100%;
         }
         button{
-            width: 100%;
+            width: 65%;
             margin: 1% 1.5%;
             padding: 2%;
             font-size: 1.5rem;
             border-radius: 30px;
             text-shadow: 1px 1px 0.2px rgb(0 0 0 / 43%);
-            box-shadow: -1px 1px 3px 0px rgb(34 60 80);
+            //box-shadow: -1px 1px 3px 0px rgb(34 60 80);
             border: none;
             background: linear-gradient(to left, #b7dfe9,#adc7e2, #a0a6d8);
             a{
