49ce53a5d7a59f597a0f73292edcafb93423c6cb
AnigalHub <anigal1997@gmail.com>
2022-12-15 00:34:40 +0300

    добавлен массив с контактами

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -2,60 +2,25 @@
     <div id="Contacts">
         <b-container>
             <b-row>
-                <b-col>
+                <b-col cols="5">
                     <component :is="svg"/>
                 </b-col>
                 <b-col>
                     <h2>Наши контакты:</h2>
-                    <p>
-                        Свяжитесь с нами и мы ответим на любой ваш вопрос:
-                    </p>
-                    <div class="text">
-                        <div class="nameContact">
-                            <b-row>
-                                <b-col cols="1">
-                                    <component :is="call"/>
-                                </b-col>
-                                <b-col>
-                                    <div class="contacts">
-                                        <span>Телефон:</span>
-                                        <br/>
-                                        <a>+7 (968) - 824 -80-96</a>
-                                    </div>
-
-                                </b-col>
-                            </b-row>
-                        </div>
-                        <div class="nameContact">
-                            <b-row>
-                                <b-col cols="1">
-                                    <component :is="mail"/>
-                                </b-col>
-                                <b-col>
-                                    <div class="contacts">
-                                        <span>Почта:</span>
-                                        <br/>
-                                        <a>mag@mail.ru</a>
-                                    </div>
-
-                                </b-col>
-                            </b-row>
-                        </div>
-                        <div class="nameContact">
-                            <b-row>
-                                <b-col cols="1">
-                                    <component :is="location"/>
-                                </b-col>
-                                <b-col>
-                                    <div class="contacts">
-                                        <span>Адрес:</span>
-                                        <br/>
-                                        <a>Новорязанское шоссе, дом 6, подъезд 9-10, этаж 2.</a>
-                                    </div>
-
-                                </b-col>
-                            </b-row>
-                        </div>
+                    <p>Свяжитесь с нами и мы ответим на любой ваш вопрос:</p>
+                    <div class="nameContact text" v-for="value in Contacts">
+                        <b-row>
+                            <b-col cols="1">
+                                <component :is="value.svg"/>
+                            </b-col>
+                            <b-col>
+                                <div class="contacts">
+                                    <span>{{value.name}}</span>
+                                    <br/>
+                                    <a>{{value.description}}</a>
+                                </div>
+                            </b-col>
+                        </b-row>
                     </div>
                 </b-col>
             </b-row>
@@ -64,7 +29,7 @@
 </template>
 
 <script>
-    import AboutSvg from "./svg/AboutSvg";
+    import ContactsSvg from "./svg/ContactsSvg";
     import CallSvg from "./svg_contacts/call";
     import MailSvg from "./svg_contacts/mail";
     import LocationSvg from "./svg_contacts/location";
@@ -72,10 +37,24 @@
         name: "Contacts",
         data() {
             return {
-                svg: AboutSvg,
-                call:CallSvg,
-                mail:MailSvg,
-                location:LocationSvg
+                svg: ContactsSvg,
+                Contacts: [
+                    {
+                        name:'Телефон:',
+                        description:'+7 (968) - 824 -80-96',
+                        svg:CallSvg,
+                    },
+                    {
+                        name:'Почта:',
+                        description:'mag@mail.ru',
+                        svg:MailSvg,
+                    },
+                    {
+                        name:'Адрес:',
+                        description:'Новорязанское шоссе, дом 6, подъезд 9-10, этаж 2.',
+                        svg:LocationSvg,
+                    },
+                ],
             }
         }
     }
@@ -87,25 +66,24 @@
         .contacts{
             padding-left: 2%;
         }
-
-               span{
-                   text-decoration: underline;
-               }
-                a{
-                    margin-top: 5%;
-                    text-decoration: none;
-                }
-
-            .nameContact{
-                padding: 1.5%;
-                border-radius: 20px;
-                svg{
-                    width: 2.5rem;
-                    margin-top: 40%;
-                    margin-right: 3%;
-                }
+        span{
+            text-decoration: underline;
+        }
+        a{
+            margin-top: 5%;
+            text-decoration: none;
+        }
+        .nameContact{
+            padding: 1.5%;
+            border-radius: 20px;
+            svg{
+                width: 2.5rem;
+                margin-top: 40%;
+                margin-right: 3%;
             }
-
-
+        }
+        svg{
+            width: 100%;
+        }
     }
 </style>
