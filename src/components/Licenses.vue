f9825e112d23f25fd322648c7ee8ae65b81fedfd
AnigalHub <anigal1997@gmail.com>
2023-06-14 01:02:56 +0300

    добавлено скачивание и просмотр (модальное окно)

--- src/components/Licenses.vue
+++ src/components/Licenses.vue
@@ -6,22 +6,25 @@
                 <b-col cols="5">
                     <component :is="svg"/>
                 </b-col>
-                <b-col >
+                <b-col>
                     <table class="table">
                         <tbody>
                             <tr v-for="license in Licenses" class="text">
                                 <td> {{license.text}}</td>
                                 <td>
-                                    <span> Просмотр</span><component :is="viewSvg"/>
+                                    <a @click="showModal(license)"><span> Просмотр</span><component :is="viewSvg"/></a>
                                 </td>
                                 <td>
-                                    <span> Скачать</span><component :is="downloadSvg"/>
+                                    <a :href="license.href" download><span>Скачать</span><component :is="downloadSvg"/></a>
                                 </td>
                             </tr>
                         </tbody>
                     </table>
                 </b-col>
             </b-row>
+            <b-modal ref="my-modal" id="img" size="lg" centered>
+                <iframe :src="selectedLicense.href" width="100%" height="100%"/>
+            </b-modal>
         </b-container>
     </div>
 </template>
@@ -36,17 +39,26 @@
             return{
                 modalShow: false,
                 svg:AdvantagesSvg,
+                selectedLicense:{
+                    href:''
+                },
                 Licenses:[
-                    {img_src:"./documents/license-1.png", text:"Телематические услуги связи",},
-                    {img_src:"./documents/license-1.png", text:"Услуги связи для целей проводного радиовещания",},
-                    {img_src:"./documents/license-1.png", text:"Услуги связи по передаче данных, за исключением услуг связи по передаче данных для целей передачи голосовой информации",},
-                    {img_src:"./documents/license-1.png", text:'Услуги связи по предоставлению каналов связи'},
-                    {img_src:"./documents/license-1.png", text:"Деятельность по монтажу, техническому обслуживанию и ремонту средств обеспечения пожарной безопасности зданий и сооружений",},
+                    {href:"./documents/Телематические услуги связи.pdf", text:"Телематические услуги связи",},
+                    {href:"./documents/Услуги связи для целей проводного радиовещания.pdf", text:"Услуги связи для целей проводного радиовещания",},
+                    {href:"./documents/Услуги связи по передаче данных, за исключением услуг связи по передаче данных для целей передачи голосовой информации.pdf", text:"Услуги связи по передаче данных, за исключением услуг связи по передаче данных для целей передачи голосовой информации",},
+                    {href:"./documents/Услуги связи по предоставлению каналов связи.pdf", text:'Услуги связи по предоставлению каналов связи'},
+                    {href:"./documents/Деятельность по монтажу, техническому обслуживанию и ремонту средств обеспечения пожарной безопасности зданий и сооружений.pdf", text:"Деятельность по монтажу, техническому обслуживанию и ремонту средств обеспечения пожарной безопасности зданий и сооружений",},
                 ],
                 viewSvg:ViewSvg,
                 downloadSvg: DownloadSvg
             }
         },
+        methods: {
+            showModal(license) {
+                this.selectedLicense = license;
+                this.$refs['my-modal'].show()
+            },
+        }
     }
 </script>
 <style scoped lang="scss">
