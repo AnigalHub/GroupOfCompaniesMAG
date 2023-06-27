07403de923ea3ea49057c65a96c75126f92432cb
AnigalHub <anigal1997@gmail.com>
2023-06-27 14:23:40 +0300

    изменен вывод направлений (цикл в цикле), удален миксин

--- src/components/Directions.vue
+++ src/components/Directions.vue
@@ -3,22 +3,16 @@
         <b-container>
             <h2>Основные направления работы компании</h2>
             <b-row>
-                <b-col>
+                <b-col v-for="directions in Directions">
                     <div>
-                        <component :is="svgDim"/>
-                        <DirectionsItems :items="Directions1"></DirectionsItems>
-                    </div>
-                </b-col>
-                <b-col>
-                    <div>
-                        <component :is="svgMonitor"/>
-                        <DirectionsItems :items="Directions2"></DirectionsItems>
-                    </div>
-                </b-col>
-                <b-col>
-                    <div>
-                        <component :is="svgEl"/>
-                        <DirectionsItems :items="Directions3"></DirectionsItems>
+                        <component :is="directions.svg"/>
+                        <div class="wrapper">
+                            <div>
+                                <ul>
+                                    <li v-for="item in directions.values">{{item.text}}</li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </b-col>
             </b-row>
@@ -30,35 +24,40 @@
     import MonitoringSvg from "./newSvg/Directions/MonitoringSvg";
     import EvacuationSvg from "./newSvg/Directions/EvacuationSvg";
     import ElectricitySvg from "./newSvg/Directions/ElectricitySvg";
-    import DirectionsItems from "@/components/mixins/DirectionsItems";
     export default {
         name: "Directions",
-        components:{
-            DirectionsItems
-        },
         data(){
             return{
-                Directions1:[
-                    {text:"Оповещения населения (ГОиЧС)",},
-                    {text:"Оповещения и управления эвакуацией (СОУЭ)",},
-                    {text:"Пожарные сигнализации (СПС, АПС)",},
-                    {text:"Проводное радиовещание (РСО, РАСЦО)",},
+                Directions:[
+                    {
+                        svg:EvacuationSvg,
+                        values:[
+                            {text:"Оповещения населения (ГОиЧС)",},
+                            {text:"Оповещения и управления эвакуацией (СОУЭ)",},
+                            {text:"Пожарные сигнализации (СПС, АПС)",},
+                            {text:"Проводное радиовещание (РСО, РАСЦО)",},
+                        ]
+                    },
+                    {
+                        svg:MonitoringSvg,
+                        values:[
+                            {text:"Каналы связи (оптические, электрические, беспроводные)",},
+                            {text:"Видеонаблюдения (СОТ, Безопасный регион, Безопасный город)",},
+                            {text:"Дератизационные системы (ОЗДС)",},
+                            {text:"Охранные комплексы и различный мониторинг",},
+                        ]
+                    },
+                    {
+                        svg:ElectricitySvg,
+                        values: [
+                            {text:"Контроль учета электроэнергии (АСКУЭ)",},
+                            {text:"Контроль и управления доступом (СКУД)",},
+                            {text:"Управления и диспетчеризация инженерных систем (АСУД)",},
+                            {text:"Электрические сети и установки (Бытовые и промышленные до 220КВ)",},
+                        ]
+                    }
+
                 ],
-                Directions2:[
-                    {text:"Каналы связи (оптические, электрические, беспроводные)",},
-                    {text:"Видеонаблюдения (СОТ, Безопасный регион, Безопасный город)",},
-                    {text:"Дератизационные системы (ОЗДС)",},
-                    {text:"Охранные комплексы и различный мониторинг",},
-                ],
-                Directions3:[
-                    {text:"Контроль учета электроэнергии (АСКУЭ)",},
-                    {text:"Контроль и управления доступом (СКУД)",},
-                    {text:"Управления и диспетчеризация инженерных систем (АСУД)",},
-                    {text:"Электрические сети и установки (Бытовые и промышленные до 220КВ)",},
-                ],
-                svgMonitor: MonitoringSvg,
-                svgDim: EvacuationSvg,
-                svgEl:ElectricitySvg,
             }
         }
     }
