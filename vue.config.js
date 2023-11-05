c9c03ad27c05f165750cb575cf93233eb808dc99
AnigalHub <anigal1997@gmail.com>
2023-11-05 22:15:32 +0300

    добавлен envMode

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
+const envMode = process.env.NODE_ENV;
+
+
+let publicPath = '/mag/'
+
+if(envMode === 'production')
+  publicPath = ''
+console.log('Before building', {envMode, publicPath})
+return -1
+
 module.exports = {
-  publicPath: '/mag/'
+  publicPath,
 }
